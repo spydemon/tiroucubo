217f2da14f6de395ef7b71b9d357093c0c8f7fb0
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2021-01-14 22:53:03 +0100

    Set up translations.

--- src/Controller/AbstractBaseController.php
+++ src/Controller/AbstractBaseController.php
@@ -10,11 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
  */
 abstract class AbstractBaseController extends AbstractController
 {
-    protected function render(string $view, array $parameters = [], Response $response = null): Response
-    {
-        $parameters = $this->addDefaultParameters($parameters);
-        return parent::render($view, $parameters, $response);
-    }
+    private ?string $pageTitle = null;
 
     protected function addDefaultParameters(array $baseParameters) : array
     {
@@ -23,12 +19,28 @@ abstract class AbstractBaseController extends AbstractController
             'page' => [
                 'author' => 'Administrator',
                 'lang' => 'en',
-                'title' => null
+                'title' => $this->getPageTitle()
             ],
             'website' => [
                 'title' => 'Spyzone'
             ]
         ];
         return array_replace_recursive($defaultParameters, $baseParameters);
+    }
+
+    protected function getPageTitle() : ?string
+    {
+        return $this->pageTitle;
+    }
+
+    protected function setPageTitle(string $title) : void
+    {
+        $this->pageTitle = $title;
+    }
+
+    protected function render(string $view, array $parameters = [], Response $response = null): Response
+    {
+        $parameters = $this->addDefaultParameters($parameters);
+        return parent::render($view, $parameters, $response);
     }
 }
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,9 +5,19 @@ namespace App\Controller\Admin;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DashboardController extends AbstractAdminController
 {
+    private TranslatorInterface $translator;
+
+    public function __construct(
+        TranslatorInterface $translator
+    ) {
+        $this->translator = $translator;
+        $this->setPageTitle($this->translator->trans('Admin dashboard'));
+    }
+
     /**
      * TODO: replace with PHP 8 annotation when available.
      * @IsGranted("ROLE_ADMIN")

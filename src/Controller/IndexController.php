58177416c020a5e4fa752c6aade52370f55819e5
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2021-01-21 16:14:27 +0100

    Set dynamically the request locale depending of the URL root on IndexController.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,21 +2,36 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractBaseController
 {
+    private RequestStack $requestStack;
+
+    public function __construct(
+        RequestStack $requestStack
+    ) {
+        $this->requestStack = $requestStack;
+    }
+
     /**
-     * @return Response
-     * @param string|null $path
      * @Route("/{path}", name="default", requirements={"path"=".*"})
      */
     public function display(string $path = null) : Response
     {
-        /**
-         * TODO: display the home page and each resource based on path.
-         */
-        return new Response('Path : ' . $path);
+        $this->setLocale();
+        return $this->render('front/path.twig.html');
+    }
+
+    protected function setLocale() : void
+    {
+        static $existingLocales = ['fr', 'en'];
+        $request = $this->requestStack->getMasterRequest();
+        preg_match('#^/(.*?)/#', $request->getRequestUri(), $matches);
+        if (isset($matches[1]) && in_array($matches[1], $existingLocales)) {
+            $request->setLocale($matches[1]);
+        }
     }
 }

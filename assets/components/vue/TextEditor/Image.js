4358ba426fcb8f3c4fe3b274d23e1c2c49af9206
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2021-02-09 22:08:19 +0100

    Enhance tiptap custom Image component in order to be able to set a caption and the alterative text on it.

--- assets/components/vue/TextEditor/Image.js
+++ assets/components/vue/TextEditor/Image.js
@@ -2,49 +2,74 @@
  * This file is a copy of this: https://github.com/ueberdosis/tiptap/issues/573#issuecomment-577534970
  */
 import { Image as TiptapImage } from "tiptap-extensions";
-import { TextSelection } from "tiptap";
 
 class Image extends TiptapImage {
+
+    // The prosemirror schema object
+    // Take a look at https://prosemirror.net/docs/guide/#schema for a detailed explanation
     get schema() {
         return {
             attrs: {
                 src: {},
-                alt: {
-                    default: ""
-                },
-                caption: {
-                    default: ""
-                }
+                alt: {},
+                caption: {}
             },
             group: "block",
             draggable: true,
             selectable: false,
+            // define how the editor will detect your node from pasted HTML
+            // every blockquote tag will be converted to this blockquote node
             parseDOM: [
                 {
-                    tag: "img[src]",
+                    // The "tag" parameter set the trigger that will activate the parsing of the DOM element by the current plugin.
+                    tag: "figure",
+                    // "getAttrs" will hydrate parameters of this javascript object by content fetched in the DOM from the element "locked" by the "tag."
                     getAttrs: dom => ({
-                        src: dom.getAttribute("src"),
-                        alt: dom.getAttribute("alt"),
-                        caption: dom.getAttribute("caption")
+                        src: dom.getElementsByTagName("img")[0].getAttribute("src") || '',
+                        alt: dom.getElementsByTagName("img")[0].getAttribute("alt") || '',
+                        caption: dom.getElementsByTagName("figcaption")[0].textContent || ''
                     })
                 }
             ],
-            toDOM: node => ["img", node.attrs]
+            // "toDOM" will generated the output HTML of this plugin.
+            toDOM: node => [
+                // <figure>
+                "figure", {}, [
+                    // <img src="{{node.attrs.src}}" />
+                    "img", {'src' : node.attrs.src, 'alt' : node.attrs.alt}],
+                    // <figcaption>{{node.attrs.caption}}</figcaption>
+                    ['figcaption', {}, node.attrs.caption]
+                // </figure>
+            ]
         };
     }
 
-    commands({ type }) {
+    // this command will be called from menus to add a blockquote
+    // `type` is the prosemirror schema object for this blockquote
+    // `schema` is a collection of all registered nodes and marks
+    commands({ type, schema }) {
         return attrs => (state, dispatch) => {
             return dispatch(state.tr.replaceSelectionWith(type.create(attrs)));
         };
     }
 
+    // return a vue component
+    // this can be an object or an imported component
     get view() {
         return {
+            // there are some props available
+            // `node` is a Prosemirror Node Object
+            // `updateAttrs` is a function to update attributes defined in `schema`
+            // `view` is the ProseMirror view instance
+            // `options` is an array of your extension options
+            // `selected` is a boolean which is true when selected
+            // `editor` is a reference to the TipTap editor instance
+            // `getPos` is a function to retrieve the start position of the node
+            // `decorations` is an array of decorations around the node
             props: ["node", "updateAttrs", "view", "getPos"],
             data() {
                 return {
-                    editor: null
+                    editor: null,
                 };
             },
             watch: {
@@ -55,54 +80,32 @@ class Image extends TiptapImage {
                 }
             },
             computed: {
+                alt: {
+                    get() {return this.node.attrs.alt;},
+                    set(alt) {this.updateAttrs({alt});}
+                },
                 src: {
-                    get() {
-                        return this.node.attrs.src;
-                    },
-                    set(src) {
-                        this.updateAttrs({
-                            src
-                        });
-                    }
+                    get() {return this.node.attrs.src;},
+                    set(src) {this.updateAttrs({src});}
                 },
                 caption: {
-                    get() {
-                        return this.node.attrs.caption;
-                    },
-                    set(caption) {
-                        this.updateAttrs({
-                            caption
-                        });
-                    }
-                }
-            },
-            mounted() {},
-            methods: {
-                captionPlaceHolder() {
-                    return "Placeholder";
-                },
-                handleKeyup(event) {
-                    let {
-                        state: { tr }
-                    } = this.view;
-                    const pos = this.getPos();
-                    if (event.key === "Backspace" && !this.caption) {
-                        let textSelection = TextSelection.create(tr.doc, pos, pos + 1);
-                        this.view.dispatch(
-                            tr.setSelection(textSelection).deleteSelection(this.src)
-                        );
-                        this.view.focus();
-                    } else if (event.key === "Enter") {
-                        let textSelection = TextSelection.create(tr.doc, pos + 2, pos + 2);
-                        this.view.dispatch(tr.setSelection(textSelection));
-                        this.view.focus();
-                    }
+                    get() {return this.node.attrs.caption;},
+                    set(caption) {this.updateAttrs({caption});}
                 }
             },
             template: `
           <figure>
             <img :src="src" />
-            <figcaption><input v-model="caption" placeholder="Type caption for image (optional)" @keyup="handleKeyup"/></figcaption>
+            <figcaption>
+              <div class="metadata">
+                  <label>Caption</label>
+                  <input v-model="caption" type="text"/>
+              </div>
+              <div class="metadata">
+                  <label>Alt</label>
+                  <input v-model="alt" type="text" />
+              </div>
+            </figcaption>
           </figure>
         `
         };

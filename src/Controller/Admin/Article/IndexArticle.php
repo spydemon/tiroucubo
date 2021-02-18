8f9f79cb043df7ea107d01795163edbcf64aba99
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2021-02-18 15:25:23 +0100

    Add HTML title on admin/article page.

--- src/Controller/Admin/Article/IndexArticle.php
+++ src/Controller/Admin/Article/IndexArticle.php
@@ -8,15 +8,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class IndexArticle extends AbstractAdminController
 {
     private ArticleRepository $articleRepository;
+    private TranslatorInterface $translator;
 
     public function __construct(
-        ArticleRepository $articleRepository
+        ArticleRepository $articleRepository,
+        TranslatorInterface $translator
     ) {
         $this->articleRepository = $articleRepository;
+        $this->translator = $translator;
+        $this->setPageTitle($this->translator->trans('Article management'));
     }
 
     /**

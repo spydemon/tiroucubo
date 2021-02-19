dac6ea9c64d47adfa2acccf8ba13493a9f7dda85
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2021-02-19 15:54:06 +0100

    Fix cache flush in acceptation tests.

--- src/Tests/Extension/PrepareDatabase.php
+++ src/Tests/Extension/PrepareDatabase.php
@@ -13,11 +13,16 @@ class PrepareDatabase implements BeforeFirstTestHook
 
     public static function resetDatabase() : void
     {
-        // We are unsetting the XDEBUG_CONFIG in order to avoid xdebug and/or PHPStorm to freeze the execution of the process if the debugger is enabled.
+        // We are unsetting the XDEBUG_CONFIG in order to avoid xdebug and/or PHPStorm to freeze the execution of the
+        // process if the debugger is enabled.
+        // This command should be useless since the next one is also supposed to clear the system cache, but it looks
+        // like that wired behavior can still occurs if we remove this entry.
+        exec('unset XDEBUG_CONFIG && bin/console cache:clear');
+        // Clear all customs and system caches.
+        exec('unset XDEBUG_CONFIG && bin/console cache:pool:clear cache.global_clearer');
         exec('unset XDEBUG_CONFIG && bin/console doctrine:database:drop -e test --force');
         exec('unset XDEBUG_CONFIG && bin/console doctrine:database:create -e test');
         exec('unset XDEBUG_CONFIG && bin/console doctrine:migrations:migrate -e test -n');
         exec('unset XDEBUG_CONFIG && bin/console doctrine:fixtures:load -e test -n');
-        exec('unset XDEBUG_CONFIG && bin/console cache:clear');
     }
 }
